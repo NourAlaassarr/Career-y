41bf17905c4fa36ec960b67c79a54600d7a2c8c2
Nour Ayman <104293377+NourAlaassarr@users.noreply.github.com>
2024-05-02 03:26:49 +0300

    Delete backend directory

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-import  express  from "express";
-import { initiateApp } from "./src/utils/initiateApp.js";
-import { config } from "dotenv"
-import path from 'path'
-config({ path: path.resolve('./config/Config.env') })
-const App = express()
-
-initiateApp(App,express)
