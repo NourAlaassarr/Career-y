ea9e7539d6375d9a5f53e63b458b8ddc9e5d3c8a
Rawan74 <rawanhelmy96@gmail.com>
2024-07-05 18:45:11 +0300

    styling

--- Frontend/src/Components/QuizPage.jsx
+++ Frontend/src/Components/QuizPage.jsx
@@ -1,101 +1,17 @@
-// // Import your quiz images
-// import CSSLogo from '/SkillsLogo/CSS.jpeg';
-// import JavaScriptLogo from '/SkillsLogo/JavaScript.png';
-// import ReactLogo from '/SkillsLogo/React.jpeg';
-// import NetworkingLogo from '/SkillsLogo/Networking_Android.webp';
-// import SecurityTestingLogo from '/SkillsLogo/Security Testing.jpg';
-// import CloudSecurityLogo from '/SkillsLogo/Cloud Security.jpg';
-// import CSharpLogo from '/SkillsLogo/Csharp.jpeg';
-// import PHPLogo from '/SkillsLogo/PHP.png';
-// import RubyLogo from '/SkillsLogo/Ruby.jpeg';
-// import JavaLogo from '/SkillsLogo/Java.png';
-// import PythonLogo from '/SkillsLogo/Python.png';
-// import PostgreSQLLogo from '/SkillsLogo/PostgreSQL.png';
-// import MySQLLogo from '/SkillsLogo/mysql.png';
-// import MariaDBLogo from '/SkillsLogo/MariaDB.png';
-// //mafi4 logo liha
-// import MSSQLLogo from '/SkillsLogo/mysql.png';
-// import OracleLogo from '/SkillsLogo/oracle.png';
-// import RESTLogo from '/SkillsLogo/REST.png';
-// //mafi4
-// import JSONAPILogo from '/SkillsLogo/oracle.png';
-// import SOAPLogo from '/SkillsLogo/SOAP.jpg';
-// import GRPCLogo from '/SkillsLogo/gRPC.png';
-// import GraphQLLogo from '/SkillsLogo/GraphQL.jpeg';
-// import ASPNETCoreLogo from '/SkillsLogo/ASP.NET Core.png';
-// import FlaskLogo from '/SkillsLogo/Flask.png';
-// import HibernateLogo from '/SkillsLogo/Hibernate.png';
-// import KoaJSLogo from '/SkillsLogo/Koa.js.jpeg';
-// import LaravelLogo from '/SkillsLogo/laravel.png';
-// import NestJSLogo from '/SkillsLogo/NestJS.png';
-// import PhoenixLogo from '/SkillsLogo/Phoenix.jpeg';
-// import RubyOnRailsLogo from '/SkillsLogo/Ruby on Rails.png';
-// import NodeJSLogo from '/SkillsLogo/Node.js.png';
-// //fih game physics
-// import GameDevelopmentFCLogo from '/SkillsLogo/Node.js.png';
-// import ReactNativeFCLogo from '/SkillsLogo/React.jpeg';
-// import SecurityLogo from '/SkillsLogo/Security Testing.jpg';
-// //testing android?
-// import TestingLogo from '/SkillsLogo/Testing_Android.png';
-// //Networking_Cyber Security.jpeg?
-// import CyberSecurityLogo from '/SkillsLogo/Networking_Cyber Security.jpeg';
-// import OperatingSystemsLogo from '/SkillsLogo/Operating Systems.jpeg';
-// import MathLogo from '/SkillsLogo/Math.jpeg';
-// import GamePhysicsLogo from '/SkillsLogo/Game Physics.jpeg';
-// import ComputerGraphicsLogo from '/SkillsLogo/Computer Graphics.jpeg';
-// import MachineLearningLogo from '/SkillsLogo/Machine Learning.jpeg';
-// import AnimationLogo from '/SkillsLogo/Animation.png';
-// import ExcelLogo from '/SkillsLogo/Excel.jpeg';
-// import TableauLogo from '/SkillsLogo/Tableau.webp';
-// import PowerBILogo from '/SkillsLogo/Power BI.jpeg';
-// import SQLLogo from '/SkillsLogo/SQL.png';
-// import DataCleaningLogo from '/SkillsLogo/Data Cleaning.png';
-// import DataCollectionLogo from '/SkillsLogo/Data Collection.png';
-// import DescriptiveAnalysisLogo from '/SkillsLogo/Descriptive Analysis.jpeg';
-// import DataVisualizationLogo from '/SkillsLogo/Data Visualization.webp';
-// import StatisticalAnalysisLogo from '/SkillsLogo/Statistical Analysis.png';
-// import BigDataLogo from '/SkillsLogo/Big Data.webp';
-// import KotlinLogo from '/SkillsLogo/Kotlin.png';
-// import GitHubLogo from '/SkillsLogo/GitHub.jpeg';
-// import ArchitectureAndDesignPatternsLogo from '/SkillsLogo/Architecture and Design Patterns.png';
-// import GitLogo from '/SkillsLogo/Git.png';
-// import MathematicsMLLogo from '/SkillsLogo/Mathematics for Machine Learning Specialization.png';
-// import StatisticsMLLogo from '/SkillsLogo/statistics for Machine Learning Specialization.png';
-// import EconometricsLogo from '/SkillsLogo/Econometrics.jpeg';
-// import ExploratoryDataAnalysisLogo from '/SkillsLogo/exploratory data analysis.jpeg';
-// import DeepLearningLogo from '/SkillsLogo/Deep learning.png';
-// import MLOpsLogo from '/SkillsLogo/MLOps.png';
-// //Security Testing.jpg
-// import BasicsSoftwareTestingFCLogo from '/SkillsLogo/Security Testing.jpg';
-// import SDLCModelsLogo from '/SkillsLogo/SDLC Models.jpeg';
-// //mafi4 manual
-// import ManualTestingLogo from '/SkillsLogo/SDLC Models.jpeg';
-// import AutomatedTestingLogo from '/SkillsLogo/Automated Testing.png';
-// import NonFunctionalTestingLogo from '/SkillsLogo/Non Functional Testing.jpeg';
-// import EmailTestingLogo from '/SkillsLogo/Email Testing.jpeg';
-// import HeadlessTestingLogo from '/SkillsLogo/Headless Testing.png';
-// //mafi4 data science
-// import DataScienceFCLogo from '/SkillsLogo/Headless Testing.png';
-// import HTMLLogo from '/SkillsLogo/HTML.png';
-// import VueLogo from '/SkillsLogo/vue.png';
-// import AngularLogo from '/SkillsLogo/Angular.png';
-// import CLogo from '/SkillsLogo/C.png';
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { httpGet } from "../axios/axiosUtils"; // Ensure the correct import path
 import "../Styles/QuizPage.css";
 
-// Import your quiz images
-import CSSLogo from "../../images/SkillsLogo/CSS.jpeg";
+import CSSLogo from "../../images/SkillsLogo/CSS.png";
 import JavaScriptLogo from "../../images/SkillsLogo/JavaScript.png";
-import ReactLogo from "../../images/SkillsLogo/React.jpeg";
-import NetworkingLogo from "../../images/SkillsLogo/Networking_Android.webp";
-import SecurityTestingLogo from "../../images/SkillsLogo/Security Testing.jpg";
-import CloudSecurityLogo from "../../images/SkillsLogo/Cloud Security.jpg";
-import CSharpLogo from "../../images/SkillsLogo/Csharp.jpeg";
+import ReactLogo from "../../images/SkillsLogo/React Native.png";
+import NetworkingLogo from "../../images/SkillsLogo/Networking_Android.png";
+import SecurityTestingLogo from "../../images/SkillsLogo/Security Testing.png";
+import CloudSecurityLogo from "../../images/SkillsLogo/Cloud Security.png";
+import CSharpLogo from "../../images/SkillsLogo/Csharp.png";
 import PHPLogo from "../../images/SkillsLogo/PHP.png";
-import RubyLogo from "../../images/SkillsLogo/Ruby.jpeg";
+import RubyLogo from "../../images/SkillsLogo/Ruby.png";
 import JavaLogo from "../../images/SkillsLogo/Java.png";
 import PythonLogo from "../../images/SkillsLogo/Python.png";
 import PostgreSQLLogo from "../../images/SkillsLogo/PostgreSQL.png";
@@ -104,62 +20,95 @@ import MariaDBLogo from "../../images/SkillsLogo/MariaDB.png";
 import MSSQLLogo from "../../images/SkillsLogo/mysql.png";
 import OracleLogo from "../../images/SkillsLogo/oracle.png";
 import RESTLogo from "../../images/SkillsLogo/REST.png";
-import JSONAPILogo from "../../images/SkillsLogo/oracle.png";
-import SOAPLogo from "../../images/SkillsLogo/SOAP.jpg";
+import JSONAPILogo from "../../images/SkillsLogo/JSON APIs.png";
+import SOAPLogo from "../../images/SkillsLogo/SOAP.png";
 import GRPCLogo from "../../images/SkillsLogo/gRPC.png";
-import GraphQLLogo from "../../images/SkillsLogo/GraphQL.jpeg";
+import GraphQLLogo from "../../images/SkillsLogo/GraphQL.png";
 import ASPNETCoreLogo from "../../images/SkillsLogo/ASP.NET Core.png";
 import FlaskLogo from "../../images/SkillsLogo/Flask.png";
 import HibernateLogo from "../../images/SkillsLogo/Hibernate.png";
-import KoaJSLogo from "../../images/SkillsLogo/Koa.js.jpeg";
+import KoaJSLogo from "../../images/SkillsLogo/Koa.js.png";
 import LaravelLogo from "../../images/SkillsLogo/laravel.png";
 import NestJSLogo from "../../images/SkillsLogo/NestJS.png";
-import PhoenixLogo from "../../images/SkillsLogo/Phoenix.jpeg";
+import PhoenixLogo from "../../images/SkillsLogo/Phoenix.png";
 import RubyOnRailsLogo from "../../images/SkillsLogo/Ruby on Rails.png";
 import NodeJSLogo from "../../images/SkillsLogo/Node.js.png";
 import GameDevelopmentFCLogo from "../../images/SkillsLogo/Node.js.png";
-import ReactNativeFCLogo from "../../images/SkillsLogo/React.jpeg";
-import SecurityLogo from "../../images/SkillsLogo/Security Testing.jpg";
+import ReactNativeFCLogo from "../../images/SkillsLogo/React Native.png";
+import SecurityLogo from "../../images/SkillsLogo/Security Testing.png";
 import TestingLogo from "../../images/SkillsLogo/Testing_Android.png";
-import CyberSecurityLogo from "../../images/SkillsLogo/Networking_Cyber Security.jpeg";
-import OperatingSystemsLogo from "../../images/SkillsLogo/Operating Systems.jpeg";
-import MathLogo from "../../images/SkillsLogo/Math.jpeg";
-import GamePhysicsLogo from "../../images/SkillsLogo/Game Physics.jpeg";
-import ComputerGraphicsLogo from "../../images/SkillsLogo/Computer Graphics.jpeg";
-import MachineLearningLogo from "../../images/SkillsLogo/Machine Learning.jpeg";
+import CyberSecurityLogo from "../../images/SkillsLogo/Networking_Cyber Security.png";
+import OperatingSystemsLogo from "../../images/SkillsLogo/Operating Systems.png";
+import MathLogo from "../../images/SkillsLogo/Math.png";
+import GamePhysicsLogo from "../../images/SkillsLogo/Game Physics.png";
+import ComputerGraphicsLogo from "../../images/SkillsLogo/Computer Graphics.png";
+import MachineLearningLogo from "../../images/SkillsLogo/Machine Learning.png";
 import AnimationLogo from "../../images/SkillsLogo/Animation.png";
-import ExcelLogo from "../../images/SkillsLogo/Excel.jpeg";
-import TableauLogo from "../../images/SkillsLogo/Tableau.webp";
-import PowerBILogo from "../../images/SkillsLogo/Power BI.jpeg";
+import ExcelLogo from "../../images/SkillsLogo/Excel.png";
+import TableauLogo from "../../images/SkillsLogo/Tableau.png";
+import PowerBILogo from "../../images/SkillsLogo/Power BI.png";
 import SQLLogo from "../../images/SkillsLogo/SQL.png";
 import DataCleaningLogo from "../../images/SkillsLogo/Data Cleaning.png";
 import DataCollectionLogo from "../../images/SkillsLogo/Data Collection.png";
-import DescriptiveAnalysisLogo from "../../images/SkillsLogo/Descriptive Analysis.jpeg";
-import DataVisualizationLogo from "../../images/SkillsLogo/Data Visualization.webp";
+import DescriptiveAnalysisLogo from "../../images/SkillsLogo/Descriptive Analysis.png";
+import DataVisualizationLogo from "../../images/SkillsLogo/Data Visualization.png";
 import StatisticalAnalysisLogo from "../../images/SkillsLogo/Statistical Analysis.png";
-import BigDataLogo from "../../images/SkillsLogo/Big Data.webp";
+import BigDataLogo from "../../images/SkillsLogo/Big Data.png";
 import KotlinLogo from "../../images/SkillsLogo/Kotlin.png";
-import GitHubLogo from "../../images/SkillsLogo/GitHub.jpeg";
+import GitHubLogo from "../../images/SkillsLogo/GitHub.png";
 import ArchitectureAndDesignPatternsLogo from "../../images/SkillsLogo/Architecture and Design Patterns.png";
 import GitLogo from "../../images/SkillsLogo/Git.png";
 import MathematicsMLLogo from "../../images/SkillsLogo/Mathematics for Machine Learning Specialization.png";
 import StatisticsMLLogo from "../../images/SkillsLogo/statistics for Machine Learning Specialization.png";
-import EconometricsLogo from "../../images/SkillsLogo/Econometrics.jpeg";
-import ExploratoryDataAnalysisLogo from "../../images/SkillsLogo/exploratory data analysis.jpeg";
+import EconometricsLogo from "../../images/SkillsLogo/Econometrics.png";
+import ExploratoryDataAnalysisLogo from "../../images/SkillsLogo/exploratory data analysis.png";
 import DeepLearningLogo from "../../images/SkillsLogo/Deep learning.png";
 import MLOpsLogo from "../../images/SkillsLogo/MLOps.png";
-import BasicsSoftwareTestingFCLogo from "../../images/SkillsLogo/Security Testing.jpg";
-import SDLCModelsLogo from "../../images/SkillsLogo/SDLC Models.jpeg";
-import ManualTestingLogo from "../../images/SkillsLogo/SDLC Models.jpeg";
+import BasicsSoftwareTestingFCLogo from "../../images/SkillsLogo/Security Testing.png";
+import SDLCModelsLogo from "../../images/SkillsLogo/SDLC Models.png";
+import ManualTestingLogo from "../../images/SkillsLogo/QA Manual Testing.png";
 import AutomatedTestingLogo from "../../images/SkillsLogo/Automated Testing.png";
-import NonFunctionalTestingLogo from "../../images/SkillsLogo/Non Functional Testing.jpeg";
-import EmailTestingLogo from "../../images/SkillsLogo/Email Testing.jpeg";
+import NonFunctionalTestingLogo from "../../images/SkillsLogo/Non Functional Testing.png";
+import EmailTestingLogo from "../../images/SkillsLogo/Email Testing.png";
 import HeadlessTestingLogo from "../../images/SkillsLogo/Headless Testing.png";
 import DataScienceFCLogo from "../../images/SkillsLogo/Headless Testing.png";
 import HTMLLogo from "../../images/SkillsLogo/HTML.png";
 import VueLogo from "../../images/SkillsLogo/vue.png";
 import AngularLogo from "../../images/SkillsLogo/Angular.png";
 import CLogo from "../../images/SkillsLogo/C.png";
+import Cplusplus from "../../images/SkillsLogo/C++.png";
+import Assembly from "../../images/SkillsLogo/Assembly.png";
+import Rust from "../../images/SkillsLogo/Rust.png";
+import EmbeddedC from "../../images/SkillsLogo/Embedded C.png";
+import ARM_Assembly from "../../images/SkillsLogo/ARM Assembly.png";
+import AVR_Assembly from "../../images/SkillsLogo/AVR Assembly.png";
+import SPI_Protocol from "../../images/SkillsLogo/SPI Protocol.png";
+import I2C_Protocol from "../../images/SkillsLogo/I2C Protocol.png";
+import UART_Protocol from "../../images/SkillsLogo/UART Protocol.png";
+import CAN_protocol from "../../images/SkillsLogo/CAN protocol .png";
+import Embedded_Linux from "../../images/SkillsLogo/Embedded Linux.png";
+import FreeRTOSpng from "../../images/SkillsLogo/FreeRTOSpng.png";
+import MicroPython from "../../images/SkillsLogo/MicroPython.png";
+import BareMetalProgramming from "../../images/SkillsLogo/Bare Metal Programming.png";
+import RealTimeOperatingSystems from "../../images/SkillsLogo/Real-Time Operating Systems.png";
+import DebuggingTechniques from "../../images/SkillsLogo/Debugging Techniques.png";
+import FirmwareDesign from "../../images/SkillsLogo/Firmware Design.png";
+import Dart from "../../images/SkillsLogo/Dart.png";
+import DesignPatterns_dart from "../../images/SkillsLogo/Design Patterns_dart.png";
+import SolidPrinciples_dart from "../../images/SkillsLogo/Solid Principles_dart.png";
+import OOP from "../../images/SkillsLogo/OOP.png";
+import SQLite from "../../images/SkillsLogo/SQLite.png";
+import Firebase from "../../images/SkillsLogo/Firebase.png";
+import AdvancedDart from "../../images/SkillsLogo/Advanced Dart.png";
+import WidgetTesting from "../../images/SkillsLogo/Widget Testing.png";
+import IntegrationTesting from "../../images/SkillsLogo/Integration Testing.png";
+import MongoDB from "../../images/SkillsLogo/MongoDB.png";
+import NoSQLDatabases from "../../images/SkillsLogo/NoSQL Databases.png";
+import DataWarehouse from "../../images/SkillsLogo/Data Warehouse.png";
+import ApacheSpark from "../../images/SkillsLogo/Apache Spark.png";
+import KafkApache from "../../images/SkillsLogo/Kafk Apache.png";
+import DatabaseSecurity from "../../images/SkillsLogo/Database Security.png";
+import RX from "../../images/SkillsLogo/Dart.png";
 
 // Mapping quiz names to their respective images
 const quizImageMap = {
@@ -236,6 +185,39 @@ const quizImageMap = {
   vue: VueLogo,
   angular: AngularLogo,
   c: CLogo,
+  "c++": Cplusplus,
+  assembly: Assembly,
+  rust: Rust,
+  "embedded c": EmbeddedC,
+  "arm assembly": ARM_Assembly,
+  "avr assembly": AVR_Assembly,
+  "spi protocol": SPI_Protocol,
+  "i2c protocol": I2C_Protocol,
+  "uart protocol": UART_Protocol,
+  "can protocol": CAN_protocol,
+  "embedded linux": Embedded_Linux,
+  freertos: FreeRTOSpng,
+  micropython: MicroPython,
+  "bare metal programming": BareMetalProgramming,
+  "real-time operating systems": RealTimeOperatingSystems,
+  "debugging techniques": DebuggingTechniques,
+  "firmware design": FirmwareDesign,
+  dart: Dart,
+  "design patterns": DesignPatterns_dart,
+  "solid principles": SolidPrinciples_dart,
+  oop: OOP,
+  sqlite: SQLite,
+  firebase: Firebase,
+  "advanced dart": AdvancedDart,
+  "widget testing": WidgetTesting,
+  "integration testing": IntegrationTesting,
+  mongodb: MongoDB,
+  "nosql databases": NoSQLDatabases,
+  "data warehouse": DataWarehouse,
+  "apache spark": ApacheSpark,
+  "apache kafka": KafkApache,
+  "database security": DatabaseSecurity,
+  rxdart: RX,
 };
 
 const QuizPage = () => {
@@ -245,7 +227,7 @@ const QuizPage = () => {
   useEffect(() => {
     const fetchQuizzes = async () => {
       try {
-        const response = await httpGet("Quiz/AllQuizzes");
+        const response = await httpGet("/Quiz/AllQuizzes");
         console.log("Quizzes fetched:", response); // Debug log
         if (response && response.Quizzes) {
           setQuizzes(response.Quizzes);
@@ -265,7 +247,7 @@ const QuizPage = () => {
   };
 
   const filteredQuizzes = quizzes.filter((quiz) =>
-    quiz.name?.toLowerCase().includes(searchTerm.toLowerCase())
+    quiz.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   const getQuizImage = (quizName) => {
@@ -285,12 +267,14 @@ const QuizPage = () => {
       <div className="quizzes-container">
         {filteredQuizzes.map((quiz) => (
           <Link key={quiz.id} to={`/quiz/${quiz.id}`} className="quiz-link">
-            <img
-              src={getQuizImage(quiz.name)}
-              alt={`${quiz.name} logo`}
-              className="quiz-logo"
-            />
-            <span>{quiz.name}</span>
+            <div className="quiz-content">
+              <img
+                src={getQuizImage(quiz.name)}
+                alt={`${quiz.name} logo`}
+                className="quiz-logo"
+              />
+              <span className="quiz-name">{quiz.name}</span>
+            </div>
           </Link>
         ))}
       </div>
