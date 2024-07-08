b64293712bb0e95dd12bca27e1631ed05c14c8d3
itsalaassar <itsalaassar@gmail.com>
2024-07-08 21:56:43 +0300

    job validations

--- Backend/src/Modules/Jobs/Job.Routes.js
+++ Backend/src/Modules/Jobs/Job.Routes.js
@@ -5,17 +5,20 @@ import { asyncHandler } from '../../utils/ErrorHandling.js';
 import  {isAuth} from'../../Middleware/auth.js'
 import {JobApis}from'./Job.endpoints.js'
 import { ValidationCoreFunction } from '../../Middleware/Validation.js'
-
+import * as JobValidation from'./Job.Validator.js'
 //Admin Only 
-router.post('/AddJob',isAuth(JobApis.Add_JobOffer),asyncHandler(JobControllers.AddJobOffer))
+router.post('/AddJob',isAuth(JobApis.Add_JobOffer),ValidationCoreFunction(JobValidation.AddJobOffer),asyncHandler(JobControllers.AddJobOffer))
 //Admin Only 
-router.delete('/DeleteJobOffer',isAuth(JobApis.DeleteJob),asyncHandler(JobControllers.DeleteJob))
+router.delete('/DeleteJobOffer',isAuth(JobApis.DeleteJob),ValidationCoreFunction(JobValidation.DeleteJob),asyncHandler(JobControllers.DeleteJob))
 //Admin Only 
 router.put('/UpdateJobOffer',isAuth(JobApis.UpdateJobOffer),asyncHandler(JobControllers.UpdateJobOffer))
 
 
 router.get('/GetAllJobOffers',isAuth(JobApis.GetAllJobOffers),asyncHandler(JobControllers.GetAllJobOffers))
-router.get('/GetTrackJobOffers',isAuth(JobApis.GetAllJobOffers),asyncHandler(JobControllers.GetAllJobOffers))
-router.get('/JobDetails',isAuth(JobApis.GetJobDetails),asyncHandler(JobControllers.GetJobDetails))
+router.get('/GetTrackJobOffers',isAuth(JobApis.GetAllJobOffers),ValidationCoreFunction(JobValidation.GetAllJobsIntrack),asyncHandler(JobControllers.GetAllJobsIntrack))
+router.get('/JobDetails',isAuth(JobApis.GetJobDetails),ValidationCoreFunction(JobValidation.JobDetails),asyncHandler(JobControllers.GetJobDetails))
+
+
+//Crons
 router.post('/SendNoti',asyncHandler(JobControllers.sendNotification))
 export default router
